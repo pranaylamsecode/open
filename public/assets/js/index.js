0a674c357a406a1d79be20558404a1befa54a287
pranaylamsecode <pranaylamsecode@gmail.com>
2024-01-29 14:27:19 +0530

    added

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -15,33 +15,9 @@ $(".next").click(function () {
 
 	//show the next fieldset
 	next_fs.show();
+	current_fs.hide();
+	animating = false;
 	//hide the current fieldset with style
-	current_fs.animate(
-		{ opacity: 0 },
-		{
-			step: function (now, mx) {
-				//as the opacity of current_fs reduces to 0 - stored in "now"
-				//1. scale current_fs down to 80%
-				scale = 1 - (1 - now) * 0.2;
-				//2. bring next_fs from the right(50%)
-				left = now * 50 + "%";
-				//3. increase opacity of next_fs to 1 as it moves in
-				opacity = 1 - now;
-				current_fs.css({
-					transform: "scale(" + scale + ")",
-					position: "absolute",
-				});
-				next_fs.css({ left: left, opacity: opacity });
-			},
-			duration: 800,
-			complete: function () {
-				current_fs.hide();
-				animating = false;
-			},
-			//this comes from the custom easing plugin
-			easing: "easeInOutBack",
-		}
-	);
 });
 
 $(".previous").click(function () {
@@ -58,33 +34,9 @@ $(".previous").click(function () {
 
 	//show the previous fieldset
 	previous_fs.show();
+	current_fs.hide();
+	animating = false;
 	//hide the current fieldset with style
-	current_fs.animate(
-		{ opacity: 0 },
-		{
-			step: function (now, mx) {
-				//as the opacity of current_fs reduces to 0 - stored in "now"
-				//1. scale previous_fs from 80% to 100%
-				scale = 0.8 + (1 - now) * 0.2;
-				//2. take current_fs to the right(50%) - from 0%
-				left = (1 - now) * 50 + "%";
-				//3. increase opacity of previous_fs to 1 as it moves in
-				opacity = 1 - now;
-				/* current_fs.css({ left: left }); */
-				previous_fs.css({
-					/* transform: "scale(" + scale + ")", */
-					opacity: opacity,
-				});
-			},
-			duration: 800,
-			complete: function () {
-				current_fs.hide();
-				animating = false;
-			},
-			//this comes from the custom easing plugin
-			easing: "easeInOutBack",
-		}
-	);
 });
 
 /* $(".previous").click(function () {
