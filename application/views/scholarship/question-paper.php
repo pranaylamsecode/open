6b3aec9f9ed652e486bd7b55adc3e921f835be06
pranaylamsecode <pranaylamsecode@gmail.com>
2024-01-31 11:16:24 +0530

    added

--- application/views/scholarship/question-paper.php
+++ application/views/scholarship/question-paper.php
@@ -161,13 +161,15 @@
 
 									<?php
 
-                                    if($quest->add_by_import == '1')
+                                    if($quest->add_by_import == '1' && !empty($quest->file))
                                     {
                                         ?>
 
+
+
                                      <img width="250px ; height:250px;" src="<?php echo $quest->file; ?>" alt="bansal-pathshala">
 
-                                    <?php   } else if(isset($quest->file)){ ?>
+                                    <?php   } else if(isset($quest->file) && !empty($quest->file)){ ?>
 
 
 
@@ -185,13 +187,13 @@
                                             <label class="radio">
                                             <?php
 
-                                        if($quest->add_by_import == '1')
+                                        if($quest->add_by_import == '1'  && !empty($quest->file))
                                         {
                                             ?>
 
                                         <img width="250px ; height:250px;" src="<?php echo $quest->file_a; ?>" alt="bansal-pathshala">
 
-                                        <?php   } else if(isset($quest->file_a)){ ?>
+                                        <?php   } else if(isset($quest->file_a)  && !empty($quest->file) ){ ?>
 
 
 
@@ -213,13 +215,13 @@
                                             <label class="radio">
 											<?php
 
-                                        if($quest->add_by_import == '1')
+                                        if($quest->add_by_import == '1'  && !empty($quest->file))
                                         {
                                             ?>
 
                                         <img width="250px ; height:250px;" src="<?php echo $quest->file_b; ?>" alt="bansal-pathshala">
 
-                                        <?php   } else if(isset($quest->file_b)){ ?>
+                                        <?php   } else if(isset($quest->file_b)  && !empty($quest->file)){ ?>
 
 
 
@@ -240,13 +242,13 @@
                                             <label class="radio">
 											<?php
 
-                                        if($quest->add_by_import == '1')
+                                        if($quest->add_by_import == '1'  && !empty($quest->file))
                                         {
                                             ?>
 
                                         <img width="250px ; height:250px;" src="<?php echo $quest->file_c; ?>" alt="bansal-pathshala">
 
-                                        <?php   } else if(isset($quest->file_c)){ ?>
+                                        <?php   } else if(isset($quest->file_c)  && !empty($quest->file) ){ ?>
 
 
 
@@ -267,13 +269,13 @@
                                             <label class="radio">
 											<?php
 
-                                        if($quest->add_by_import == '1')
+                                        if($quest->add_by_import == '1'  && !empty($quest->file))
                                         {
                                             ?>
 
                                         <img width="250px ; height:250px;" src="<?php echo $quest->file_d; ?>" alt="bansal-pathshala">
 
-                                        <?php   } else if(isset($quest->file_d)){ ?>
+                                        <?php   } else if(isset($quest->file_d)  && !empty($quest->file)){ ?>
 
 
 
