ad69d67d7fae04c8a5219d547362d7157605bba9
pranaylamsecode <pranaylamsecode@gmail.com>
2024-02-03 22:23:01 +0530

    added

--- admin/application/controllers/Report.php
+++ admin/application/controllers/Report.php
@@ -112,6 +112,13 @@ function examMarkReport3($exam_id = null, $student_id = null, $student_id2 = nul
         $page_data['exam_id']       =  $this->input->post('exam_id');
         $page_data['student_id']    =  $this->input->post('student_id');
         $page_data['student_id2']    =  $this->input->post('student_id2');
+        $page_data['batch']    =  $this->input->post('batch');
+
+        if(!empty($page_data['batch']) && !empty($page_data['student_id']))
+        {
+            redirect(base_url(). 'report/examMarkReportBatch/'. $page_data['exam_id'] .'/'.$page_data['student_id'] .'/'.$page_data['student_id2'] , 'refresh');
+
+        }else
 
         if(!empty($page_data['student_id2']) && !empty($page_data['student_id'])){
 
@@ -144,8 +151,13 @@ function examMarkReport4($exam_id = null, $student_id = null, $student_id2 = nul
         $page_data['exam_id']       =  $this->input->post('exam_id');
         $page_data['student_id']    =  $this->input->post('student_id');
         $page_data['student_id2']    =  $this->input->post('student_id2');
+        $page_data['batch']    =  $this->input->post('batch');
 
-        if(!empty($page_data['student_id2']) && !empty($page_data['student_id'])){
+        if(!empty($page_data['batch']) && !empty($page_data['student_id']))
+        {
+            redirect(base_url(). 'report/examMarkReportBatch/'. $page_data['exam_id'] .'/'.$page_data['student_id'] .'/'.$page_data['student_id2'] , 'refresh');
+
+        }elseif(!empty($page_data['student_id2']) && !empty($page_data['student_id'])){
 
             redirect(base_url(). 'report/examMarkReport4/'. $page_data['exam_id'] .'/'.$page_data['student_id'] .'/'.$page_data['student_id2'] , 'refresh');
         }elseif(!empty($page_data['exam_id'])){
@@ -165,6 +177,43 @@ function examMarkReport4($exam_id = null, $student_id = null, $student_id2 = nul
         $page_data['student_id']       =   $student_id;
         $page_data['student_id2']       =   $student_id2;
         $page_data['page_name']     =   'examMarkReport3';
+        $page_data['page_title']    = get_phrase('Quiz Marks');
+        $this->load->view('backend/index', $page_data);
+}
+
+function examMarkReportBatch($exam_id = null, $student_id = null, $student_id2 = null){
+
+    if($this->input->post('operation') == 'selection'){
+
+        $page_data['exam_id']       =  $this->input->post('exam_id');
+        $page_data['student_id']    =  $this->input->post('student_id');
+        $page_data['student_id2']    =  $this->input->post('student_id2');
+        $page_data['batch']    =  $this->input->post('batch');
+
+        if(!empty($page_data['batch']) && !empty($page_data['student_id']))
+        {
+            redirect(base_url(). 'report/examMarkReportBatch/'. $page_data['exam_id'] .'/'.$page_data['student_id'] .'/'.$page_data['student_id2'] , 'refresh');
+
+        }elseif(!empty($page_data['student_id2']) && !empty($page_data['student_id'])){
+
+            redirect(base_url(). 'report/examMarkReport4/'. $page_data['exam_id'] .'/'.$page_data['student_id'] .'/'.$page_data['student_id2'] , 'refresh');
+        }elseif(!empty($page_data['exam_id'])){
+
+            redirect(base_url(). 'report/examMarkReport3/'. $page_data['exam_id'] .'/'.$page_data['student_id'] .'/'.$page_data['student_id2'] , 'refresh');
+
+        }
+        else{
+            $this->session->set_flashdata('error_message', get_phrase('Pleasen select something'));
+            redirect(base_url(). 'report/examMarkReport3', 'refresh');
+        }
+    }
+
+
+
+        $page_data['exam_id']       =   $exam_id;
+        $page_data['student_id']       =   $student_id;
+        $page_data['student_id2']       =   $student_id2;
+        $page_data['page_name']     =   'examMarkReportbatch';
         $page_data['page_title']    = get_phrase('Quiz Marks');
         $this->load->view('backend/index', $page_data);
 }
--- admin/application/views/backend/admin/examMarkReportbatch.php
+++ admin/application/views/backend/admin/examMarkReportbatch.php
@@ -7,7 +7,7 @@
 
                     <!----CREATION FORM STARTS---->
 
-                	<?php echo form_open(base_url() . 'report/examMarkReport3' , array('class' => 'form-horizontal form-groups-bordered validate','target'=>'_top', 'enctype' => 'multipart/form-data'));?>
+                	<?php echo form_open(base_url() . 'report/examMarkReport4' , array('class' => 'form-horizontal form-groups-bordered validate','target'=>'_top', 'enctype' => 'multipart/form-data'));?>
 
                             <div class="form-group">
                                     <label class="col-md-12" for="example-text"><?php echo get_phrase('Quiz');?></label>
@@ -65,9 +65,7 @@ $student_data = $this->db->get()->result_array();
                                         <?php endforeach;?>
                                     </select>
                                 </div>
-
                             </div>
-                            <!-- <input type="text" name="batch" value="1"> -->
 
                             <?php
                                             }?>
@@ -161,6 +159,21 @@ $json_data = json_encode($formatted_result);
 
 
 
+$query2 = $this->db->query("SELECT UNIX_TIMESTAMP(created_at) as date, quiz_id, score as value FROM quiz_report WHERE student_id = $student_id2 AND quiz_id = $exam_id");
+
+$result2 = $query2->result_array();
+
+// Convert the result into the desired format
+$formatted_result2 = array_map(function($row) {
+    return [
+        'date' => $row['date'] * 1000, // Multiply by 1000 to convert seconds to milliseconds
+        'value' => (int)$row['value']
+    ];
+}, $result2);
+
+// Now $formatted_result contains the data in the desired format
+$json_data2 = json_encode($formatted_result2);
+
 ?>
 
 
@@ -187,106 +200,17 @@ $json_data = json_encode($formatted_result);
 
 <?php endif; ?>
 
-<script>
-    /**
-     * ---------------------------------------
-     * This demo was created using amCharts 5.
-     *
-     * For more information visit:
-     * https://www.amcharts.com/
-     *
-     * Documentation is available at:
-     * https://www.amcharts.com/docs/v5/
-     * ---------------------------------------
-     */
 
-    // Create root element
-    // https://www.amcharts.com/docs/v5/getting-started/#Root_element
-    var root = am5.Root.new("chartdiv");
+<?php if(!empty($student_id2)){ ?>
+    <script>
+    // ... (existing code)
 
-    const myTheme = am5.Theme.new(root);
-
-    myTheme.rule("AxisLabel", ["minor"]).setAll({
-        dy: 1
-    });
-
-    // Set themes
-    // https://www.amcharts.com/docs/v5/concepts/themes/
-    root.setThemes([
-        am5themes_Animated.new(root),
-        myTheme,
-        am5themes_Responsive.new(root)
-    ]);
-
-    // Create chart
-    // https://www.amcharts.com/docs/v5/charts/xy-chart/
-    var chart = root.container.children.push(am5xy.XYChart.new(root, {
-        panX: false,
-        panY: false,
-        wheelX: "panX",
-        wheelY: "zoomX",
-        paddingLeft: 0
-    }));
-
-    // Add cursor
-    // https://www.amcharts.com/docs/v5/charts/xy-chart/cursor/
-    var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {
-        behavior: "zoomX"
-    }));
-    cursor.lineY.set("visible", false);
-
-    var date = new Date();
-    date.setHours(0, 0, 0, 0);
-    var value = 100;
-
-    function generateData() {
-        value = Math.round((Math.random() * 10 - 5) + value);
-        am5.time.add(date, "day", 1);
-        return {
-            date: date.getTime(),
-            value: value
-        };
-    }
-
-    function generateDatas(count) {
-        var data = [];
-        for (var i = 0; i < count; ++i) {
-            data.push(generateData());
-        }
-        return data;
-    }
-
-    // Create axes
-    // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
-    var xAxis = chart.xAxes.push(am5xy.DateAxis.new(root, {
-        maxDeviation: 0,
-        baseInterval: {
-            timeUnit: "day",
-            count: 1
-        },
-        renderer: am5xy.AxisRendererX.new(root, {
-            minorGridEnabled: true,
-            minorLabelsEnabled: true
-        }),
-        tooltip: am5.Tooltip.new(root, {})
-    }));
-
-    xAxis.set("minorDateFormats", {
-        "day": "dd",
-        "month": "MM"
-    });
-
-    var yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
-        renderer: am5xy.AxisRendererY.new(root, {})
-    }));
-
-    // Add series
-    // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
-    var series = chart.series.push(am5xy.LineSeries.new(root, {
-        name: "Series",
+    // Add series for batch comparison
+    var comparisonSeries = chart.series.push(am5xy.LineSeries.new(root, {
+        name: "Comparison",
         xAxis: xAxis,
         yAxis: yAxis,
-        valueYField: "value",
+        valueYField: "comparisonValue",
         valueXField: "date",
         tooltip: am5.Tooltip.new(root, {
             labelText: "{valueY}"
@@ -299,19 +223,35 @@ $json_data = json_encode($formatted_result);
         orientation: "horizontal"
     }));
 
-    var data = generateDatas(30);
-    var dataset = <?php echo $json_data; ?>;
-    console.log(dataset);
-    series.data.setAll(dataset);
+    // Fetch and set data for the main series (current batch)
+    var currentBatchData = <?php echo $json_data; ?>;
+    series.data.setAll(currentBatchData);
+
+    // Fetch and set data for comparison series (other batches)
+    var comparisonDatasets = [
+        <?php echo $json_data2; ?>,
+        <?php echo $json_data3; ?>, // Add more datasets as needed
+        // ...
+    ];
+
+    // Merge all comparison datasets into a single array
+    var comparisonData = [];
+    comparisonDatasets.forEach(function (dataset) {
+        comparisonData = comparisonData.concat(dataset);
+    });
+
+    // Set the comparison series data
+    comparisonSeries.data.setAll(comparisonData);
 
     // Make stuff animate on load
     // https://www.amcharts.com/docs/v5/concepts/animations/
     series.appear(1000);
-    chart.appear(1000, dataset.length);
+    comparisonSeries.appear(1000);
+    chart.appear(1000, currentBatchData.length);
 </script>
-<!-- normal js end  -->
 
 
+<?php } ?>
 
 <!-- campaire js end  -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
