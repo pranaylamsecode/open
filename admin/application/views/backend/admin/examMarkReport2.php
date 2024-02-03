af3f4e85c7c9e41923249564ca0324477bdfee05
pranaylamsecode <pranaylamsecode@gmail.com>
2024-02-03 18:50:27 +0530

    added

--- admin/application/views/backend/admin/examMarkReport2.php
+++ admin/application/views/backend/admin/examMarkReport2.php
@@ -131,141 +131,127 @@ $json_data = json_encode($formatted_result);
 <div id="chartdiv"></div>
 
 <?php endif; ?>
-
 <script>
     /**
- * ---------------------------------------
- * This demo was created using amCharts 5.
- *
- * For more information visit:
- * https://www.amcharts.com/
- *
- * Documentation is available at:
- * https://www.amcharts.com/docs/v5/
- * ---------------------------------------
- */
+     * ---------------------------------------
+     * This demo was created using amCharts 5.
+     *
+     * For more information visit:
+     * https://www.amcharts.com/
+     *
+     * Documentation is available at:
+     * https://www.amcharts.com/docs/v5/
+     * ---------------------------------------
+     */
 
-// Create root element
-// https://www.amcharts.com/docs/v5/getting-started/#Root_element
-var root = am5.Root.new("chartdiv");
+    // Create root element
+    // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+    var root = am5.Root.new("chartdiv");
 
-const myTheme = am5.Theme.new(root);
+    const myTheme = am5.Theme.new(root);
 
-myTheme.rule("AxisLabel", ["minor"]).setAll({
-  dy:1
-});
+    myTheme.rule("AxisLabel", ["minor"]).setAll({
+        dy: 1
+    });
 
-// Set themes
-// https://www.amcharts.com/docs/v5/concepts/themes/
-root.setThemes([
-  am5themes_Animated.new(root),
-  myTheme,
-  am5themes_Responsive.new(root)
-]);
+    // Set themes
+    // https://www.amcharts.com/docs/v5/concepts/themes/
+    root.setThemes([
+        am5themes_Animated.new(root),
+        myTheme,
+        am5themes_Responsive.new(root)
+    ]);
 
+    // Create chart
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/
+    var chart = root.container.children.push(am5xy.XYChart.new(root, {
+        panX: false,
+        panY: false,
+        wheelX: "panX",
+        wheelY: "zoomX",
+        paddingLeft: 0
+    }));
 
-// Create chart
-// https://www.amcharts.com/docs/v5/charts/xy-chart/
-var chart = root.container.children.push(am5xy.XYChart.new(root, {
-  panX: false,
-  panY: false,
-  wheelX: "panX",
-  wheelY: "zoomX",
-  paddingLeft:0
-}));
+    // Add cursor
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/cursor/
+    var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {
+        behavior: "zoomX"
+    }));
+    cursor.lineY.set("visible", false);
 
+    var date = new Date();
+    date.setHours(0, 0, 0, 0);
+    var value = 100;
 
-// Add cursor
-// https://www.amcharts.com/docs/v5/charts/xy-chart/cursor/
-var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {
-  behavior: "zoomX"
-}));
-cursor.lineY.set("visible", false);
+    function generateData() {
+        value = Math.round((Math.random() * 10 - 5) + value);
+        am5.time.add(date, "day", 1);
+        return {
+            date: date.getTime(),
+            value: value
+        };
+    }
 
-var date = new Date();
-date.setHours(0, 0, 0, 0);
-var value = 100;
+    function generateDatas(count) {
+        var data = [];
+        for (var i = 0; i < count; ++i) {
+            data.push(generateData());
+        }
+        return data;
+    }
 
-function generateData() {
-  value = Math.round((Math.random() * 10 - 5) + value);
-  am5.time.add(date, "day", 1);
-  return {
-    date: date.getTime(),
-    value: value
-  };
-}
+    // Create axes
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
+    var xAxis = chart.xAxes.push(am5xy.DateAxis.new(root, {
+        maxDeviation: 0,
+        baseInterval: {
+            timeUnit: "day",
+            count: 1
+        },
+        renderer: am5xy.AxisRendererX.new(root, {
+            minorGridEnabled: true,
+            minorLabelsEnabled: true
+        }),
+        tooltip: am5.Tooltip.new(root, {})
+    }));
 
-function generateDatas(count) {
-  var data = [];
-  for (var i = 0; i < count; ++i) {
-    data.push(generateData());
-  }
-  return data;
-}
+    xAxis.set("minorDateFormats", {
+        "day": "dd",
+        "month": "MM"
+    });
 
+    var yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
+        renderer: am5xy.AxisRendererY.new(root, {})
+    }));
 
-// Create axes
-// https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
-var xAxis = chart.xAxes.push(am5xy.DateAxis.new(root, {
-  maxDeviation: 0,
-  baseInterval: {
-    timeUnit: "day",
-    count: 1
-  },
-  renderer: am5xy.AxisRendererX.new(root, {
-    minorGridEnabled:true,
-    minorLabelsEnabled:true
-  }),
-  tooltip: am5.Tooltip.new(root, {})
-}));
+    // Add series
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
+    var series = chart.series.push(am5xy.LineSeries.new(root, {
+        name: "Series",
+        xAxis: xAxis,
+        yAxis: yAxis,
+        valueYField: "value",
+        valueXField: "date",
+        tooltip: am5.Tooltip.new(root, {
+            labelText: "{valueY}"
+        })
+    }));
 
-xAxis.set("minorDateFormats", {
-  "day":"dd",
-  "month":"MM"
-});
+    // Add scrollbar
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/scrollbars/
+    chart.set("scrollbarX", am5.Scrollbar.new(root, {
+        orientation: "horizontal"
+    }));
 
-
-var yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
-  renderer: am5xy.AxisRendererY.new(root, {})
-}));
-
-
-// Add series
-// https://www.amcharts.com/docs/v5/charts/xy-chart/series/
-var series = chart.series.push(am5xy.ColumnSeries.new(root, {
-  name: "Series",
-  xAxis: xAxis,
-  yAxis: yAxis,
-  valueYField: "value",
-  valueXField: "date",
-  tooltip: am5.Tooltip.new(root, {
-    labelText: "{valueY}"
-  })
-}));
-
-series.columns.template.setAll({ strokeOpacity: 0 })
-
-
-// Add scrollbar
-// https://www.amcharts.com/docs/v5/charts/xy-chart/scrollbars/
-chart.set("scrollbarX", am5.Scrollbar.new(root, {
-  orientation: "horizontal"
-}));
-
-var data = generateDatas(30);
-var dataset = <?php echo $json_data; ?>;
+    var data = generateDatas(30);
+    var dataset = <?php echo $json_data; ?>;
     console.log(dataset);
-series.data.setAll(dataset);
+    series.data.setAll(dataset);
 
-
-
-
-
-
-// Make stuff animate on load
-// https://www.amcharts.com/docs/v5/concepts/animations/
-series.appear(1000);
-chart.appear(1000, dataset.length);
+    // Make stuff animate on load
+    // https://www.amcharts.com/docs/v5/concepts/animations/
+    series.appear(1000);
+    chart.appear(1000, dataset.length);
 </script>
 
 
