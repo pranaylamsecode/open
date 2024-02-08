efa4ff419d095801c831bcf44be77fac29def2e3
pranaylamsecode <pranaylamsecode@gmail.com>
2024-02-08 14:49:27 +0530

    added

--- admin/application/views/backend/admin/examMarkReport3.php
+++ admin/application/views/backend/admin/examMarkReport3.php
@@ -28,10 +28,10 @@
                                                 ?>
  <?php
 
-$this->db->select('s.name as name, s.student_id, q.quiz_id as quiz_id');
-$this->db->from('student s');
+$this->db->select('s.fullName as name, s.student_id, q.quiz_id as quiz_id');
+$this->db->from('quiz_enquiry s');
 
-$this->db->join('quiz_answer q', 's.student_id = q.user_id', 'right');
+$this->db->join('quiz_answer q', 's.student_id = q.student_id', 'right');
 $this->db->where('q.quiz_id', $exam_id);
 $this->db->group_by('s.student_id');
 
@@ -79,7 +79,7 @@ $student_data = $this->db->get()->result_array();
  <?php
 
 $this->db->select('s.name as name, s.student_id, q.quiz_id as quiz_id');
-$this->db->from('student s');
+$this->db->from('quiz_enquiry s');
 
 $this->db->join('quiz_answer q', 's.student_id = q.user_id', 'right');
 $this->db->where('q.quiz_id', $exam_id);
